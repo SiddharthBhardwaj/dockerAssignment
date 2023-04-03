83249c18faac9034e97685d0127a94edfa146e89
SiddharthBhardwaj <bhrsiddharth94@gmail.com>
2023-04-04 00:05:40 +0530

    Update app.py

--- backend/app.py
+++ backend/app.py
@@ -1,14 +1,54 @@
 from flask import Flask, jsonify, request
 from datetime import datetime
+import time
 app = Flask(__name__)
 
 # square root of function
 @app.route('/square/<int:num>', methods = ['GET'])
 def square(num):
-    metric_start = datetime.now()
+    metric_start = time.time()
     square_value = num**2
-    metric_end  = datetime.now()
-    return jsonify({'data': square_value})
+    metric_end  = time.time()
+    metric_diff = metric_end - metric_start
+    return jsonify({'value': square_value,'time_taken': metric_diff})
+
+@app.route('/cube/<int:num>', methods = ['GET'])
+def cube(num):
+    metric_start = time.time()
+    square_value = num**3
+    metric_end  = time.time()
+    metric_diff = metric_end - metric_start
+    return jsonify({'value': square_value,'time_taken': metric_diff})
+
+@app.route('/fibonacci/<int:nterms>', methods = ['GET'])
+def fibonacci(nterms):
+    metric_start = time.time()
+    fib = {}
+    n1, n2 = 0, 1
+    count = 0
+
+    # check if the number of terms is valid
+    if nterms <= 0:
+        fib.append(999999)
+    # if there is only one term, return n1
+    elif nterms == 1:
+        fib.append(n1)
+    # generate fibonacci sequence
+    else:
+        print("Fibonacci sequence:")
+        while count < nterms:
+            fib.append(n1)
+            nth = n1 + n2
+            # update values
+            n1 = n2
+            n2 = nth
+            count += 1
+    metric_end  = time.time()
+    metric_diff = metric_end - metric_start
+    if fib[0]==999999:
+        return jsonify({'value': "Please enter a positive value",'time_taken': metric_diff})
+    else:
+        return jsonify({'value': fib,'time_taken': metric_diff})
 
 # driver function
 if __name__ == '__main__':
